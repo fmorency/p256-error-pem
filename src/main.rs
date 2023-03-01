93c870d9203d992fa728b9571ffb32a7147a8f7f
Felix C. Morency <1102868+fmorency@users.noreply.github.com>
2023-03-01 15:38:57 -0500

    Enhancement

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
-const PKCS8_PRIVATE_KEY_PEM: &str = include_str!("key/id-2.pem");
+const PKCS8_PRIVATE_KEY_PEM_GOOD: &str = include_str!("key/good.pem");
+const PKCS8_PRIVATE_KEY_PEM_BAD: &str = include_str!("key/bad.pem");
 
 fn main() -> Result<(), String> {
-    let _secret_key = PKCS8_PRIVATE_KEY_PEM.parse::<p256::SecretKey>().map_err(|e| e.to_string())?;
+    println!("Parsing good.pem");
+    let _ = PKCS8_PRIVATE_KEY_PEM_GOOD.parse::<p256::SecretKey>().map_err(|e| e.to_string())?;
+    println!("Parsing good.pem succeeded.");
+
+    println!("Parsing bad.pem");
+    let _ = PKCS8_PRIVATE_KEY_PEM_BAD.parse::<p256::SecretKey>().map_err(|e| e.to_string())?;
+    println!("Parsing bad.pem succeeded.");
     Ok(())
 }
